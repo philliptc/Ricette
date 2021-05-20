80ea1ab016c17840c14c1efb536c8e5dfa2a71fb
philliptc <phill.tc05@gmail.com>
2021-05-20 14:18:39 +0700

    DetailRecipeFragment added

---
+++ app/src/main/java/com/example/ricette/fragments/DetailRecipeFragment.kt
@@ -0,0 +1,12 @@
+package com.example.ricette.fragments
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ricette.R
+
+class DetailRecipeFragment : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_recipe_fragment)
+    }
+}
