f355cd350840dbd3bafb498f4c1c299478daff7a
philliptc <phill.tc05@gmail.com>
2021-05-23 00:11:20 +0700

    open cam done (masih error)

--- app/src/main/java/com/example/ricette/fragments/AddRecipeFragment.kt
+++ app/src/main/java/com/example/ricette/fragments/AddRecipeFragment.kt
@@ -1,15 +1,26 @@
 package com.example.ricette.fragments
 
+import android.app.Activity
+import android.content.ContentResolver
+import android.content.ContentValues
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
+import android.provider.SyncStateContract.Helpers.insert
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
+import androidx.core.content.ContextCompat.checkSelfPermission
 import com.example.ricette.DataObjectRecipe
 import com.example.ricette.R
+import java.util.jar.Manifest
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -29,8 +40,13 @@ class AddRecipeFragment : Fragment() {
     private lateinit var etRecipeName : EditText
     private lateinit var etIngridients : EditText
     private lateinit var etMethods : EditText
-    private lateinit var btnRecipePicture : Button
+    private lateinit var ivRecipePicture : ImageView
     private lateinit var btnAddRecipe: Button
+    private lateinit var btnOpenGallery : Button
+    private lateinit var btnOpenCamera : Button
+    private val PERMISSION_CODE = 1000;
+    private val IMAGE_PICTURE_CODE = 1001
+    var image_uri : Uri? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,24 +64,82 @@ class AddRecipeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_add_recipe, container, false)
     }
 
+    private fun openCamera(){
+        val values = ContentValues()
+        values.put(MediaStore.Images.Media.TITLE, "New Picture")
+        values.put(MediaStore.Images.Media.DESCRIPTION, "From the camera")
+
+        image_uri = ContentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,values)
+        //camera intent
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        cameraIntent.putExtra (MediaStore.EXTRA_OUTPUT, image_uri)
+        startActivityForResult(cameraIntent, IMAGE_PICTURE_CODE)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        // dipanggil ketika user menekan ALLOW atau DENY
+        when (requestCode){
+            PERMISSION_CODE -> {
+                if (grantResults.size > 0 && grantResults [0] ==
+                        PackageManager.PERMISSION_GRANTED){
+                    //permission from popup was granted
+                    openCamera()
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        //called when image was captured from camera intent
+        if (resultCode == Activity.RESULT_OK) {
+            // set image capture to image view
+            ivRecipePicture.setImageURI(image_uri)
+        }
+    }
+
+
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         etRecipeName = view.findViewById(R.id.etFormAddRecipeName)
         etIngridients = view.findViewById(R.id.etFormAddIngredients)
         etMethods = view.findViewById(R.id.etFormAddMethods)
-        btnRecipePicture = view.findViewById(R.id.btnAddRecipePicture)
+        ivRecipePicture = view.findViewById(R.id.ivRecipePicture)
         btnAddRecipe = view.findViewById(R.id.btnAddFragmentAddRecipe)
+        btnOpenGallery = view.findViewById(R.id.btnGalleryAddRecipePicture)
+        btnOpenCamera = view.findViewById(R.id.btnCameraAddRecipePicture)
 
 
-        btnRecipePicture.setOnClickListener {
+        btnOpenGallery.setOnClickListener {
             val gallery = Intent()
             gallery.type = "image/*"
             gallery.action = Intent.ACTION_GET_CONTENT
             startActivityForResult(gallery, 100)
-
-            //open camera belom
         }
+
+        btnOpenCamera.setOnClickListener {
+//            request runtime permission
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                if (checkSelfPermission (Manifest.permission.OPEN_CAMERA)
+                == PackageManager.PERMISSION_DENIED ||
+                        checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                == PackageManager.PERMISSION_DENIED) {
+                    // permission ditolak
+                    val permission = arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    // nampilin popup request permission
+                    requestPermissions(permission, PERMISSION_CODE)
+                }
+                else{
+                    openCamera()
+                }
+            }
+            else{
+                openCamera()
+            }
+        }
+
 
         btnAddRecipe.setOnClickListener {
             val recipeName = etRecipeName.text.toString()
@@ -88,9 +162,6 @@ class AddRecipeFragment : Fragment() {
                 commit()
             }
         }
-
-
-
     }
 
     companion object {
