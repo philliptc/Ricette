f8f2608b8c7223b69daaa04e4830d6e87a590046
daveradin753 <daveputraradin@gmail.com>
2021-05-24 07:21:51 +0700

    edit recipe

--- app/src/main/java/com/example/ricette/fragments/EditRecipeFragment.kt
+++ app/src/main/java/com/example/ricette/fragments/EditRecipeFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import com.example.ricette.R
 
 
@@ -18,16 +20,58 @@ class EditRecipeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_edit_recipe, container, false)
     }
 
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val etFormEditRecipeName = view.findViewById<EditText>(R.id.etFormEditRecipeName)
+        val etFormEditIngredients = view.findViewById<EditText>(R.id.etFormEditIngredients)
+        val etFormEditMethods = view.findViewById<EditText>(R.id.etFormEditMethods)
+        val btnEdit = view.findViewById<Button>(R.id.btnEditFragmentAddRecipe)
+        val btnGalleryEditRecipePicture = view.findViewById<Button>(R.id.btnGalleryEditRecipePicture)
+        val btnCameraEditRecipePicture = view.findViewById<Button>(R.id.btnCameraEditRecipePicture)
+        val btnBackEditRecipeFragment = view.findViewById<Button>(R.id.btnBackEditRecipeFragment)
 
         val recipeName = arguments?.getString("recipeName")
         val recipeIngridients = arguments?.getString("recipeIngridients")
         val recipeMethods = arguments?.getString("recipeMethods")
         val recipePictureUri = arguments?.getString("recipePictureUri")
 
+        etFormEditRecipeName.setText(recipeName)
+        etFormEditIngredients.setText(recipeIngridients)
+        etFormEditMethods.setText(recipeMethods)
+
+        btnGalleryEditRecipePicture.setOnClickListener {
+
+        }
+
+        btnCameraEditRecipePicture.setOnClickListener {
+
+        }
+
+        btnEdit.setOnClickListener {
+
+        }
+        btnBackEditRecipeFragment.setOnClickListener {
+            val fragmentManager = fragmentManager
+            val detailRecipeFragment = DetailRecipeFragment()
+            val bundle = Bundle()
+            bundle.putString("recipeName", recipeName)
+            bundle.putString("recipeIngridients", recipeIngridients)
+            bundle.putString("recipeMethods", recipeMethods)
+            bundle.putString("recipePictureUri", recipePictureUri)
+            detailRecipeFragment.arguments = bundle
+
+            fragmentManager?.beginTransaction()?.apply {
+                replace(R.id.flMainActivity, detailRecipeFragment)
+                remove(this@EditRecipeFragment)
+                commit()
+            }
+        }
 
     }
+
+
 
     companion object {
 
